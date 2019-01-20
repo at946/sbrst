55d731a1af536151b9df54a305779c9c94a16265
Asato TAKAHASHI <asato730@gmail.com>
2019-01-20 23:10:19 +0900

    change limit time 30s to 10s

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,6 +6,6 @@ class Setting
   validates :problem, presence: true
   validates :limit_time, presence: true
   validates :limit_time, numericality: {greater_than_or_equal_to: 1, message: "は1分以上に設定してください"}
-  validates :limit_time, numericality: {less_than_or_equal_to: 30, message: "は30分以下に設定してください"}
+  validates :limit_time, numericality: {less_than_or_equal_to: 10, message: "は10分以下に設定してください"}
 
 end
